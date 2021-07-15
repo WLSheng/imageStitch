38b1da5d65f2a8e8cb90c24be3cc898832b7cc44
sheng <wulisheng@heils.cn>
2021-07-15 17:49:06 +0800

    添加功能：清理三天前的拼接缓存图

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import base64
 import cv2
 import time
 import traceback
+import os
+import datetime
 
 app = flask.Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024
@@ -31,6 +33,35 @@ def imageStitch():
         pic = pic.decode()
         returndata = {"status": '0', "Pic": pic}
         print(" =====  拼接结束  ======")
+        try:
+            print("开始清理三天前的拼接图")
+            dir_list = os.listdir("./cache/")
+            if not dir_list:
+                print("没有找到缓存的拼接图")
+            else:
+                for i, one_result_name in enumerate(dir_list):
+                    print(i, one_result_name)
+                    img_path = os.path.join(r'./cache/', one_result_name)
+                    today = datetime.datetime.now()
+                    # 计算偏移量,前3天
+                    offset = datetime.timedelta(days=-3)
+                    # 获取想要的日期的时间,即前3天时间
+                    re_date = (today + offset)
+                    # 前3天时间转换为时间戳
+                    re_date_unix = time.mktime(re_date.timetuple())
+
+                    file_time = os.path.getmtime(img_path)  # 文件修改时间
+                    timeArray = time.localtime(file_time)  # 时间戳->结构化时间
+                    otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)  # 格式化时间
+                    print("文件修改时间", otherStyleTime)
+                    if file_time <= re_date_unix:
+                        print(f"已经超过3天,需要删除,delete:{img_path}")
+                        os.remove(img_path)
+                    else:
+                        print("未超过3天,无需处理!!!")
+
+        except:
+            print(f"清理三天前的拼接图失败，报错：{traceback.format_exc()}")
         return json.dumps(returndata)
     except Exception as e:
         print(" 不知道的bug:", traceback.format_exc())
